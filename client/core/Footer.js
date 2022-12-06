048657930940e2e89c7cb923cc4b3b9f4ca677e7
Kevin Jin <kevjin2002@gmail.com>
2022-12-06 08:21:00 -0600

    Added contact

--- client/core/Footer.js
+++ client/core/Footer.js
@@ -1,51 +1,51 @@
-import React from 'react';
+import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Divider from '@material-ui/core/Divider';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import Add from '@material-ui/icons/Add';
+import Divider from "@material-ui/core/Divider";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import Add from "@material-ui/icons/Add";
 
-import './Footer.css'
+import "./Footer.css";
 
 const useStyles = makeStyles((theme) => ({
-  footer_container :  {
-    paddingTop : "30px",
-    backgroundColor : "#97B6E4",
-    display : 'flex',
-    flexDirection : 'column'
+  footer_container: {
+    paddingTop: "30px",
+    backgroundColor: "#97B6E4",
+    display: "flex",
+    flexDirection: "column",
   },
-  copyright : {
-    backgroundColor : "#3f4771",
-    color : "#FFFFFF",
-    borderTopColor : 'black',
-    borderTopStyle : 'solid',
-    borderTopWidth : '4px'
+  copyright: {
+    backgroundColor: "#3f4771",
+    color: "#FFFFFF",
+    borderTopColor: "black",
+    borderTopStyle: "solid",
+    borderTopWidth: "4px",
   },
-  info_container : {
-    marginLeft : '170px',
-    marginRight : '150px',
-    marginBottom : '20px',
-    display : 'flex'
+  info_container: {
+    marginLeft: "170px",
+    marginRight: "150px",
+    marginBottom: "20px",
+    display: "flex",
   },
-  about_us :{
-    flex : '1',
-    marginLeft : '20px',
-    marginRight : '20px'
+  about_us: {
+    flex: "1",
+    marginLeft: "20px",
+    marginRight: "20px",
   },
-  memberships : {
-    flex : '1',
-    marginLeft : '20px',
-    marginRight : '20px',
-    display : 'flex',
-    flexDirection : 'column'
+  memberships: {
+    flex: "1",
+    marginLeft: "20px",
+    marginRight: "20px",
+    display: "flex",
+    flexDirection: "column",
   },
-  contact_us : {
-    flex : '1',
-    marginLeft : '20px',
-    marginRight : '20px'
-  }
+  contact_us: {
+    flex: "1",
+    marginLeft: "20px",
+    marginRight: "20px",
+  },
 }));
 
 const Footer = withRouter(function ({ history }) {
@@ -55,65 +55,61 @@ const Footer = withRouter(function ({ history }) {
     <div className={classes.footer_container}>
       <div className={classes.info_container}>
         <div className={classes.about_us}>
-          <Typography variant='h6' style={{ color : 'white'}}>
+          <Typography variant="h6" style={{ color: "white" }}>
             About Us
           </Typography>
-          <Divider style={{ marginTop : '5px', marginBottom : '20px'}}/>
+          <Divider style={{ marginTop: "5px", marginBottom: "20px" }} />
 
-          <Typography  variant='body2' style={{ color : 'black', marginTop : '10px'}} >
-          CONSAMS - The “Consortium of New Sub-Sahara African Medical Schools”
-          - was founded in 2011 under the original name of the “Consortium of 
-          New Southern African Medical Schools.” At an ensuing Annual General Meeting, 
-          it was decided to expand the organization to include members across the African 
-          continent and beyond southern - hence the name change from “Southern African” 
-          to Sub-Sahara African.
+          <Typography
+            variant="body2"
+            style={{ color: "black", marginTop: "10px" }}
+          >
+            CONSAMS - The “Consortium of New Sub-Sahara African Medical Schools”
+            - was founded in 2011 under the original name of the “Consortium of
+            New Southern African Medical Schools.” At an ensuing Annual General
+            Meeting, it was decided to expand the organization to include
+            members across the African continent and beyond southern - hence the
+            name change from “Southern African” to Sub-Sahara African.
           </Typography>
         </div>
 
         <div className={classes.memberships}>
-          <Typography variant='h6' style={{ color : 'white'}}>
+          <Typography variant="h6" style={{ color: "white" }}>
             Memberships
           </Typography>
-          <Divider style={{ marginTop : '5px', marginBottom : '20px'}}/>
+          <Divider style={{ marginTop: "5px", marginBottom: "20px" }} />
 
-          <Link style={{ flex : '1'}}>
+          <Link style={{ flex: "1" }}>
             <ArrowForwardIosIcon />
-            <Typography variant='button' >
-              About Memberships
-            </Typography>
+            <Typography variant="button">About Memberships</Typography>
           </Link>
-          <br/>
-          <Link style={{ flex : '1'}}>
+          <br />
+          <Link style={{ flex: "1" }}>
             <ArrowForwardIosIcon />
-            <Typography variant='button'>
-              Membership Fees
-            </Typography>
+            <Typography variant="button">Membership Fees</Typography>
           </Link>
-          <br/>
-          <Link style={{ flex : '1'}}>
+          <br />
+          <Link style={{ flex: "1" }}>
             <ArrowForwardIosIcon />
-            <Typography variant='button'>
-              Benefits
-            </Typography>
+            <Typography variant="button">Benefits</Typography>
           </Link>
-
         </div>
 
         <div className={classes.contact_us}>
-          <Typography variant='h6' style={{ color : 'white'}}>
+          <Typography variant="h6" style={{ color: "white" }}>
             Contact Us
           </Typography>
-          <Divider style={{ marginTop : '5px', marginBottom : '20px'}}/>
+          <Divider style={{ marginTop: "5px", marginBottom: "20px" }} />
 
-          <Typography style={{marginBottom : '5px'}}>
+          <Typography style={{ marginBottom: "5px" }}>
             <strong>Phone :</strong>
             <br />
-            Mobile : +1(111)-111-1111
+            Mobile : +1 (202)-555-0110
             <br />
-            Office : +1(111)-111-1111
+            Office : +1 (651)-555-0142
           </Typography>
 
-          <br/>
+          <br />
 
           <Typography>
             <strong>Email :</strong>
@@ -124,12 +120,12 @@ const Footer = withRouter(function ({ history }) {
       </div>
 
       <div className={classes.copyright}>
-        <Typography variant='caption' style={{ marginLeft : '30px'}}>
+        <Typography variant="caption" style={{ marginLeft: "30px" }}>
           © 2022 CONSAMS. All Rights Reserved.
         </Typography>
       </div>
     </div>
-  )
+  );
 });
 
-export default Footer
+export default Footer;
