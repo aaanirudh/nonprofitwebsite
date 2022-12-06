712c0b94116400c1b3deff5e672817162a04468f
Anirudh Balasubramanian <anirudh.balasubramanian@vanderbilt.edu>
2022-12-06 07:00:41 -0600

    add paypal button

--- client/homeviews/HomeView.js
+++ client/homeviews/HomeView.js
@@ -87,12 +87,14 @@ export default function HomeView() {
           </Typography>
 
           <Grid item>
-            {/* <Link> */}
-            <Button className={classes.payPalButton} variant="contained">
-              <FontAwesomeIcon icon={faPaypal} style={{ marginRight: "5px" }} />
-              <strong>{"Donate with PayPal"}</strong>
-            </Button>
-            {/* </Link> */}
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="business" value="FWXL7WZWFJNAY" />
+              <input type="hidden" name="no_recurring" value="0" />
+              <input type="hidden" name="item_name" value="Donate to CONSAMS to support the advancement of medical schools, and join a multinational community of medical professionals!" />
+              <input type="hidden" name="currency_code" value="USD" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
           </Grid>
         </Container>
       </div>
